7a5235c0964a91a320c29671aab235be013b96e4
navin barange <navin@engagebay.co>
2023-04-10 10:35:44 +0530

    spelling mistake

--- src/component/CreditComponent.js
+++ src/component/CreditComponent.js
@@ -1,7 +1,7 @@
 function CreditComponent() {
     return ( 
         <>
-             <div class=" text-light position-absolute bottom-0 end-0 m-4">Devloped by <a role="button"  href="https://www.nrknavin.in" target="_blank" className="link-warning cursor-pointer text-decoration-none">NRk</a></div>
+             <div class=" text-light position-absolute bottom-0 end-0 m-4">Developed by <a role="button"  href="https://www.nrknavin.in" target="_blank" className="link-warning cursor-pointer text-decoration-none">NRk</a></div>
         </>
      );
 }
